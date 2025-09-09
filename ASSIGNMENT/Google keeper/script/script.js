591b2e9b7e7a08111a166c9e40cdb988bce0e6a1
student widesoftech <nikhilshende957@gmail.com>
2025-09-09 11:36:21 +0530

    add project

--- ASSIGNMENT/Google keeper/script/script.js
+++ ASSIGNMENT/Google keeper/script/script.js
@@ -1,6 +1,6 @@
-const addNotesBtn = document.getElementById("add-notes-button");
-const popup = document.querySelector(".add-notes-pop-up");
-const closeBtn = document.querySelector(".add-notes-form .close");
+let addNotesBtn = document.getElementById("add-notes-button");
+let popup = document.querySelector(".add-notes-pop-up");
+let closeBtn = document.querySelector(".add-notes-form .close");
 
 addNotesBtn.addEventListener("click", () => {
     popup.style.display = "flex"; // show popup
