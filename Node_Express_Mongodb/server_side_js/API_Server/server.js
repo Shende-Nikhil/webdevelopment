99177d80af20975fbfca16d11858b9509aa72545
student widesoftech <nikhilshende957@gmail.com>
2025-10-12 13:38:32 +0530

    add mongodb

--- Node_Express_Mongodb/server_side_js/API_Server/server.js
+++ Node_Express_Mongodb/server_side_js/API_Server/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
+import "./database/conn.js"
 import { router } from "./routers/router.js"
+import mongoose from "mongoose"
+import { userRouter } from "./routers/userRouter.js"
 
 dotenv.config({ path: "./config.env" })
 
@@ -10,9 +13,15 @@ let port = process.env.PORT || 5005
 
 app.use(express.urlencoded({ extended: true }))
 
+app.use(express.json())
+
 app.use(express.static("public"))
 
+app.get('/', (req, res) => { res.redirect("/languages/api/get-details") })
+
 app.use("/languages/api", router)
+
+app.use("/user/api", userRouter)
 
 app.use((req, res) => {
     console.log("someone is trying to access a 404 route !")
@@ -22,3 +31,7 @@ app.use((req, res) => {
 app.listen(port, () => {
     console.log(`server is running on port ${port} !`)
 })
+
+// [GET/POST/PUT/PATCH/DELET ] /endpoint/
+
+// file uploads, mailer
